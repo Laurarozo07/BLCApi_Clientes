e92d0efc7de0d371dd93bb9ad5ea5a4cb671c32a
SergioHGHomez <90337735+SergioHGHomez@users.noreply.github.com>
2021-11-12 12:15:46 -0500

    cambiada la base de datos por DB_Clientes

--- server/conexion.js
+++ server/conexion.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
-const mongo_uri = "mongodb://localhost/test";
+const mongo_uri = "mongodb://localhost/DB_Clientes";
 
 
 mongoose.connect(mongo_uri)
     .then(db => console.log("conexion exitosa"))
-    .catch(err =>console.error(err));
-    module.exports = mongoose;
+    .catch(err => console.error(err));
+module.exports = mongoose;
